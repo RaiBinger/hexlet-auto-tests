8e0d94546042445ba92ae197083932a0ed4bf989
Alexander Romanov <raibinger@gmail.com>
2023-06-30 16:17:58 +0300

    add new test to tests

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,13 +1,13 @@
 import { capitalize } from '../src/capitalize.js';
 import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает не верно!');
-}
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает не верно!');
+// }
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает не верно!');
-}
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает не верно!');
+// }
 
 assert.equal(capitalize(''), '');
 
